f81a3437e3bf4beac8fe5d649095f657c8b129cc
Danijel <mail@danijel.io>
2017-12-01 17:35:27 +0100

    ISSUE#40: add player lobby view
    * add lobby where players wait until game begins
    * only view (no logic) is done so far

---
+++ client/app/src/main/java/de/lmu/settlebattle/catanclient/LobbyActivitiy.java
@@ -0,0 +1,14 @@
+package de.lmu.settlebattle.catanclient;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class LobbyActivitiy extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_lobby);
+	}
+
+}
